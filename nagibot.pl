322a8b7198452c84e5a7cb8b2f47fb7d70a3abf1
Richard Schwab <r.schwab@ixmid.com>
2018-11-21 05:50:10 +0100

    Allow adressing the bot using botname,

--- nagibot.pl
+++ nagibot.pl
@@ -214,7 +214,7 @@ $connection->reg_cb (
                 if (my $me = $room->get_me) {
                     $my_name = lc $me->nick;
                 }
-                my $cmd = $1 if $msg->any_body =~ /^(?:\@$my_name|$my_name:)\s+(.*)$/i;
+                my $cmd = $1 if $msg->any_body =~ /^(?:\@$my_name|$my_name[:,])\s+(.*)$/i;
                 return unless $cmd;
                 &SetIdlePresence(0);
                 &SendRoomMessage($room, &ProcessBotCommand ($msg->from, $cmd));
